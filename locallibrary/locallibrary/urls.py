34931f5bf66f3efc073b42b1cb3da831d32dbe5a
Brennan Krutis <krutisbs@dukes.jmu.edu>
2025-10-13 14:29:27 -0400

    Create Skeleton Layout for Knowledge Base

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -2,7 +2,7 @@
 URL configuration for locallibrary project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,13 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-# Use include() to add paths from the catalog application
 from django.urls import include
-
 urlpatterns += [
     path('catalog/', include('catalog.urls')),
 ]
